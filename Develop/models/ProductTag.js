121c6429a32363b238236777b31ebf62e67b944a
Kevin U <owner@Kevins-MacBook-Pro.local>
2022-09-10 19:56:22 -0700

    finished adding models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -5,7 +5,31 @@ const sequelize = require("../config/connection");
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {},
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This is a reference to product model
+        model: "Product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This is a reference to tag model
+        model: "Tag",
+        key: "id",
+      },
+    },
+  },
   {
     sequelize,
     timestamps: false,
